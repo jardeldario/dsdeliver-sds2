cb12296663455a7661f695d7e12690ca6bbe11b1
Jardel Dário <jardel.dario@gmail.com>
2021-01-12 00:02:27 -0300

    Add axios e list orders

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Text, Alert } from 'react-native';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { fetchOrders } from '../api';
+import { Order } from '../types';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoading ? (
+          <Text>Buscando pedidos...</Text>
+        ) : (
+            orders.map(order => (
+              <TouchableWithoutFeedback key={order.id}>
+                <OrderCard order={order} />
+              </TouchableWithoutFeedback>
+            ))
+          )}
       </ScrollView>
     </>
   );
