1ce2848c030604aaabb8687cff99a177f1d47a23
Jardel Dário <jardel.dario@gmail.com>
2021-01-11 23:12:59 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -3,8 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -19,9 +18,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
-      <Home />
+      <Routes />
     </View>
   );
 }
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import Header from '../Header';
 
 function Home() {
 
-  const handleOnPres = () => {
+  const navigation = useNavigation();
 
+  const handleOnPres = () => {
+    navigation.navigate('Orders');
   }
 
   return (
     <>
+      <Header />
       <View style={styles.container}>
         <Image source={require('../assets/deliveryman.png')} />
         <Text style={styles.title}>
